ac488d7128b2e36c9246237f5073fdd21db163c3
Sai Yarra <sai.yarra@uppsteamhyd.com>
2018-02-16 10:52:11 +0530

    displaying list using services (Heroes component)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,11 +14,20 @@ export class HeroesComponent implements OnInit {
   //   name:'Rajinikath'
     
   // };
-  heroes=HEROES;
-
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+  heroes:Hero[];
   ngOnInit() {
+    this.getHeroes();
   }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  // getHeroes():void{
+  //   this.heroService.getHeroes();
+  // }
 
   // selectedhero:Hero;
 
@@ -28,7 +37,7 @@ export class HeroesComponent implements OnInit {
   // }
   selectedHero:Hero;
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+  this.selectedHero = hero;
   }
 
 
